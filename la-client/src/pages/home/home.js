d336fa7db8162404eef2db70f429a5385841b55d
whackingMUFN <46834593+whackingMUFN@users.noreply.github.com>
2021-03-04 10:04:39 -0600

    added browserrouter and route to home page

---
+++ la-client/src/pages/home/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./home-styles.scss";
+
+function Home() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    )
+}
+
+export default Home;
